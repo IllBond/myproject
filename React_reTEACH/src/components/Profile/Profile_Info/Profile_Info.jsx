c64368fd171817379326fa054891f915903627a5
IlyaBondar <prince.dp.u@gmail.com>
2019-11-22 22:34:02 +0200

    перед продолжением рефакторинга профиля

--- React_reTEACH/src/components/Profile/Profile_Info/Profile_Info.jsx
+++ React_reTEACH/src/components/Profile/Profile_Info/Profile_Info.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import s from './Profile_Info.module.css'
 import x from './../Profile.module.css'
 import Prelaoder from "../../Preloader/Preloader";
 import ProfileStatusHook from "../ProfileStatusHook";
 
 
+
 let Profile_Info = (props) => {
 
+    let [editMode, ToggleEditMode] = useState(false)
     const loadFile = (e) => {
 
         if (e.target.files.length) {
@@ -28,32 +30,66 @@ let Profile_Info = (props) => {
             </div>
             <ProfileStatusHook status={props.status} updateStatusThunk={props.updateStatusThunk}/>
 
-            <div>
-                <h3>Информация обо мне</h3>
-                <div className={s.citata}>
-                    <div><strong>Меня зовут</strong>, <span
-                        className={x.status}>{props.data.fullName ? props.data.fullName : 'Тайна'}</span></div>
-                    <div><strong>Обо мне</strong>, <span
-                        className={x.status}>{props.data.aboutMe ? props.data.aboutMe : 'Ничего...'}</span></div>
-                    <div> > <span
-                        className={x.status}>{!props.data.lookingForAJob ? 'Я ищу работу' : 'У меня уже есть работа'}</span>
-                    </div>
-                    <div><strong>Мои нвыки</strong>, <span
-                        className={x.status}>{props.data.lookingForAJobDescription ? props.data.lookingForAJobDescription : 'Пока никаких'}</span>
-                    </div>
-                    <div className={x.statusContact}>
-                        <b>Мои контакты:</b>
-                        <Contact contacts={props.data.contacts} />
-                    </div>
-                </div>
-            </div>
+            {editMode ? <AllProfileInfo_EditMode ToggleEditMode={ToggleEditMode} isYou={props.isYou}  data={props.data}/> : <AllProfileInfo isYou={props.isYou}  ToggleEditMode={ToggleEditMode} data={props.data} />}
+
         </div>
     }
 }
 
+let AllProfileInfo = (props) => {
+
+    return <div>
+        <h3>Информация обо мне</h3>
+        {props.isYou || <button onClick={()=>{props.ToggleEditMode(true)}}>Редактировать</button> }
+        <div className={s.citata}>
+            <div><strong>Меня зовут</strong>, <span
+                className={x.status}>{props.data.fullName ? props.data.fullName : 'Тайна'}</span></div>
+            <div><strong>Обо мне</strong>, <span
+                className={x.status}>{props.data.aboutMe ? props.data.aboutMe : 'Ничего...'}</span></div>
+            <div> > <span
+                className={x.status}>{!props.data.lookingForAJob ? 'Я ищу работу' : 'У меня уже есть работа'}</span>
+            </div>
+            <div><strong>Мои нвыки</strong>, <span
+                className={x.status}>{props.data.lookingForAJobDescription ? props.data.lookingForAJobDescription : 'Пока никаких'}</span>
+            </div>
+            <div className={x.statusContact}>
+                <b>Мои контакты:</b>
+                <Contact contacts={props.data.contacts} />
+            </div>
+        </div>
+    </div>
+};
+
+let AllProfileInfo_EditMode = (props) => {
+
+    return <div>
+        <h3>Информация обо мне</h3>
+        {props.isYou || <button onClick={()=>{props.ToggleEditMode(false)}}>Сохранить</button>}
+
+
+        <form className={s.citata}>
+            <div><strong>Меня зовут</strong>, <input type="text" value={props.data.fullName}/></div>
+            <div><strong>Обо мне</strong>, <input type="text" value={props.data.aboutMe}/></div>
+            <div>>Ищу работу? <input type="checkbox" checked={props.data.lookingForAJob}/></div>
+            <div><strong>Мои нвыки</strong>, <input type="text" value={props.data.lookingForAJobDescription}/></div>
+            <div className={x.statusContact}>
+                <b>Мои контакты:</b>
+                <ContactEdit contacts={props.data.contacts} />
+            </div>
+        </form>
+    </div>
+};
+
+
 let Contact = (props) => {
     return <div>
         {Object.keys(props.contacts).map(key => <div key={key}> {key} : {props.contacts[key]?props.contacts[key]:'пусто'}</div>)}
+    </div>
+}
+
+let ContactEdit = (props) => {
+    return <div>
+        {Object.keys(props.contacts).map(key => <div key={key}> {key} : <input value={props.contacts[key]}/></div>)}
     </div>
 }
 
